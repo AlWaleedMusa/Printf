fcf80fefef9b6ae1aad00b727acc270728ccae29
AlWaleed Musa <jako_249@hotmail.com>
2023-11-11 01:25:12 +0200

    added some #includes headers

--- main.h
+++ main.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
-int _putchar(char c);
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/* int _putchar(char c);*/ 
+/* i think we should use write() function since putchar is not mentioned on the authorized functions section + write is easier to deal with in my opinion. */
+
 int _printf(const char *format, ...);
 
 #endif
