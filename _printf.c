b08a2cb19c0c221807937e1b17cb8a13af99015d
AlWaleed Musa <jako_249@hotmail.com>
2023-11-11 01:32:28 +0200

    added a v1 implementation of the _printf function

---
+++ _printf.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _printf - a function that take a format and print it
+ * on the stdout, can take unlimited amount of args and can
+ * process different types of format.
+ * @format: format passed to the function to process.
+ * @...: args passed to the function.
+ * Return: an int.
+*/
+int _printf(const char *format, ...)
+{
+	int counter = 0;
+
+	va_list args_passed; //...
+	va_start(args_passed, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			counter += handelSpecifier(*(format++), args_passed); // working on this function
+		}
+		else
+		{
+			counter += write(1, format, 1);
+		}
+		format++;
+	}
+	return (counter);
+}
