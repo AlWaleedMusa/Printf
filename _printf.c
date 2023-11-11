099c63645f1499ab273ca740cdaa0579352d32ab
AlWaleed Musa <jako_249@hotmail.com>
2023-11-11 21:38:55 +0200

    refactoring code

--- _printf.c
+++ _printf.c
@@ -13,7 +13,7 @@ int _printf(const char *format, ...)
 	int counter = 0;
 	va_list args_passed; /*...*/
 
-	if (format == NULL)
+	if (format == NULL || (*format == '%' && *(format++) == '\0'))
 		return (-1);
 
 	va_start(args_passed, format);
