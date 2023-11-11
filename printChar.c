c542a3e50d51e4abd829403e34566bd497550413
AlWaleed Musa <jako_249@hotmail.com>
2023-11-11 18:21:02 +0200

    added a function to handel chars

---
+++ printChar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * printChar - print character to stdout.
+ * @ch: char passed form _printf.
+ * Return: an int, (number of printed characters). 
+*/
+int printfChar(int ch)
+{
+	return write(1, &ch, 1);
+}
