28ae05c48aaf1f61080c77a57d6408994059fa6c
AlWaleed Musa <jako_249@hotmail.com>
2023-11-11 17:57:23 +0200

    added some functions prototypes

--- main.h
+++ main.h
@@ -10,5 +10,7 @@
 /* i think we should use write() function since putchar is not mentioned on the authorized functions section + write is easier to deal with in my opinion. */
 
 int _printf(const char *format, ...);
+int handelSpecifier(char specifier, va_list args_passed);
+int printfString(char *string);
 
 #endif
