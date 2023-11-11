9599868045a560ce7ebfd9500ac208f2cb110f54
AlWaleed Musa <jako_249@hotmail.com>
2023-11-11 17:59:11 +0200

    added a function to handel specifiers when found

---
+++ handelSpecifier.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * handelSpecifier - handel passed character and determine the
+ * right function to call accordingly.
+ * @specifier: char passed to the function from the _printf function.
+ * @args_passed: arguments passed on _printf.
+ * Return: an int (number of printed characters).
+*/
+int handelSpecifier(char specifier, va_list args_passed)
+{
+	int counter = 0;
+
+	if (specifier == 's')
+	{
+		counter += printfString(va_arg(args_passed, char *));
+	}
+	return (counter);
+}
