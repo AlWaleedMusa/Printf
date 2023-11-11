8f690215459e7597cb6e736be6b048e0ed214ba3
AlWaleed Musa <jako_249@hotmail.com>
2023-11-11 17:59:47 +0200

    added a function to print strings

---
+++ printString.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * printfString - print strings passed to it.
+ * @string: pointer to the string to print.
+ * Return: int (number of printed characters)
+*/
+int printfString(char *string)
+{
+	return write(1, string, strlen(string));
+}
