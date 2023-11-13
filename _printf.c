0b79e6639e48b5f5113ea4368c547bccea6faaa5
AlWaleed Musa <jako_249@hotmail.com>
2023-11-13 10:59:31 +0200

    check 18 attempt / removed failed format size condition

--- _printf.c
+++ _printf.c
@@ -18,12 +18,6 @@ int _printf(const char *format, ...)
 	if (format == NULL)
 		return (-1);
 
-	size = strlen(format);
-	if (size <= 0)
-	{
-		return (-1);
-	}
-
 	if (format[0] == '%' && format[1] == '\0')
 		return (-1);
 
